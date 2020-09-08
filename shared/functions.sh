89b9c3db7943b3773a116184fc759e8eef60e11f
Rob Linger <rob.linger@gmail.com>
2020-09-07 21:55:03 -0400

    silencing hdate warinings

--- shared/functions.sh
+++ shared/functions.sh
@@ -3,8 +3,8 @@ sundial () {
   echo "Sunrise time file does not exist. Fetching sunrise/sunset times now."
   lat=$(head -1 ${workpath}location.txt)
   long=$(tail -1 ${workpath}location.txt)
-  hdate -s -l $lat -L $long -z $tzone | grep 'sunrise' | grep -o '.....$' > ${workpath}sunrise.txt
-  hdate -s -l $lat -L $long -z $tzone | grep 'sunset' | grep -o '.....$' > ${workpath}sunset.txt
+  hdate -s -l $lat -L $long -z $tzone -q| grep 'sunrise' | grep -o '.....$' > ${workpath}sunrise.txt
+  hdate -s -l $lat -L $long -z $tzone -q| grep 'sunset' | grep -o '.....$' > ${workpath}sunset.txt
   sunrise=$(cat ${workpath}sunrise.txt)
   sunset=$(cat ${workpath}sunset.txt)
   echo "Done - sunrise at $sunrise, sunset at $sunset"
@@ -30,3 +30,24 @@ lights_on() {
 					*) echo "Python light script unhandled exit code $ec";;
 				esac
 }
+
+set_time() {
+  if ping -c 1 www.google.com &> /dev/null
+	then
+		i=1
+		echo "Setting time"
+        	sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+
+                if [ ! -f "${workpath}sunrise.txt" ]; then
+
+                        sundial
+
+                fi
+
+	else
+		echo "Network not ready, exiting"
+		sleep 2
+		exit
+	fi
+}
+
