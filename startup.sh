8d1977fc9f5779336dddc28cf393f10028c7b86b
th3walkingdud3 <rob.linger@gmail.com>
2020-11-11 22:52:18 -0500

    fix python call to be consistant

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 source /home/pi/shared/settings.sh
 
 #geolocation
-curl -s https://ipinfo.io/ip | curl -s https://api.ipgeolocationapi.com/geolocate/$(</dev/stdin) |python -c 'import json,sys;obj=json.load(sys.stdin);print obj["geo"]["latitude"];print obj["geo"]["longitude"]'> ${workpath}location.txt
+curl -s https://ipinfo.io/ip | curl -s https://api.ipgeolocationapi.com/geolocate/$(</dev/stdin) |python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["geo"]["latitude"]);print (obj["geo"]["longitude"])'> ${workpath}location.txt
 #Create our state file if it doesn't exist
 echo "off" > ${workpath}light_state.log
 #Create our state file if it doesn't exist
