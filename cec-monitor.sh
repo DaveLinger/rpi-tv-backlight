083e90fb6d877a5247a582a14c879ed1874bcb7e
Dave <22969871+DaveLinger@users.noreply.github.com>
2021-01-28 19:26:25 -0500

    Update cec-monitor.sh
    Add repo name to path

--- cec-monitor.sh
+++ cec-monitor.sh
@@ -1,1 +1,1 @@
-. /home/pi/startup.sh && /usr/bin/cec-client -m -f /trash/cec-monitor.log > /dev/null 2>&1
+. /home/pi/rpi-tv-backlight/startup.sh && /usr/bin/cec-client -m -f /trash/cec-monitor.log > /dev/null 2>&1
